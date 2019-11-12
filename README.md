dd04903df59b970c3071009a87f3e530288f8e9a
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-12 11:32:11 -0700

    Called the hacker API and collected the data, added error handling and displayed the data

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
     1. https://www.udemy.com/course/universal-react-with-nextjs-the-ultimate-guide/learn/lecture/12505146#overview
     2. https://github.com/kgallagher52/next-hacker
+    3. Hacker News API: https://news.ycombinator.com/
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react'
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
 
- class Index extends Component {
-  render() {
-    return (
-      <div>
-        Hacker Next
-      </div>
-    )
+const Index = ({ stories }) => {
+	return stories.length === 0 ? (
+		<Error statusCode={503} />
+	) : (
+		<div>
+			<h1>Hacker Next </h1>
+			{stories.map((s) => <h2 key={s.id}>{s.title}</h2>)}
+		</div>
+	);
+};
+
+Index.getInitialProps = async (context) => {
+  let stories;
+	try {
+    var res = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+    stories = await res.json()
+	} catch (err) {
+		console.error(err);
+		stories = [];
   }
-}
-export default Index
+	return { stories };
+};
+
+export default Index;
