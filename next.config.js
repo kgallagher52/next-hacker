991f5763c202e35558d1567fee01194d13abc7f6
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-13 16:05:13 -0700

    Added a service worker and a custom server to integrate with the current server so that I could tell the server to use the service worker.

--- next.config.js
+++ next.config.js
@@ -1,28 +1,21 @@
-//https://www.udemy.com/course/universal-react-with-nextjs-the-ultimate-guide/learn/lecture/12505170#overview
-
-// Need to use a comment JS to bring in the package needed
-const SWPrecachWebpackPlugin = require('sw-precache-webpack-plugin');
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 
 module.exports = {
-  // webpack property exposes a function that gives us access to the configuration of the webpack. 
   webpack: config => {
-    //config.pluging.push - Will allow us to push new plugins = [] into our webpack
     config.plugins.push(
-      new SWPrecachWebpackPlugin({
-         //Configuration objects
-         //Minify our serviceWorker.js
+      new SWPrecacheWebpackPlugin({
         minify: true,
-        //Files we want to ignore
         staticFileGlobsIgnorePatterns: [/\.next\//],
-        //Caching stratagy
-        runtimeCaching:  [
+        runtimeCaching: [
           {
-            handler: 'networkFirst',
+            handler: "networkFirst",
             urlPattern: /^https?.*/
           }
         ]
       })
-    )
+    );
+
     return config;
   }
-}
+};
+
